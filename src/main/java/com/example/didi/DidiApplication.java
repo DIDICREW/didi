d3f32bd286f99cda15b6643a783f93752f522dc3
안찬웅 <104750924+AnChanUng@users.noreply.github.com>
2023-10-08 01:02:49 +0900

    test

--- src/main/java/com/example/didi/DidiApplication.java
+++ src/main/java/com/example/didi/DidiApplication.java
@@ -2,12 +2,11 @@ package com.example.didi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class DidiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DidiApplication.class, args);
 	}
-
 }
